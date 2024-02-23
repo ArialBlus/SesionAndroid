ffa9678ba735f7a7074ae4341121d4244b8811ca
blus <guardadoaltamirano.8@gmail.com>
2024-02-22 19:33:43 -0600

    ejercicio de pasar

---
+++ pass-Activity/app/src/main/java/com/example/passactivity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.passactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void siguiente(View view){
+        Intent siguinete =new Intent(this, SegundoActivity.class);
+        startActivity(siguinete);
+    }
+
+}
